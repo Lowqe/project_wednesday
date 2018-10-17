be377c2aae0afaab2e95c81b32c88dd3106ec085
TinyKenny <Jonathan@rawet.se>
2018-10-17 18:22:53 +0200

    making it possible for stuff to die

---
+++ project_wednesday/Assets/Scripts/BaseEnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int GetExperienceReward() //redo this later...
+    {
+        return 0;
+    }
+
+    public void Die()
+    {
+        Debug.Log("Make enemy deahts do things!");
+        Destroy(gameObject);
+    }
+}
---
+++ project_wednesday/Assets/Scripts/Destructible.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    private int MaxHealth;
+    private int Health;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMaxHealth(int NewMaxHealth)
+    {
+        MaxHealth = NewMaxHealth;
+        Health = MaxHealth;
+    }
+
+
+
+    public void TakeGeneralDamage(int DamageToTake)
+    {
+        Health -= DamageToTake;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        BaseEnemyScript EnemyScript = gameObject.GetComponent<BaseEnemyScript>();
+        if (EnemyScript != null)
+        {
+            Debug.Log("Give experience to the most recent attacker");
+            EnemyScript.Die();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
