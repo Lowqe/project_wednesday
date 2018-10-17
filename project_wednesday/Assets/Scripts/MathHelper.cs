92861b83e82e2e0ae7cb2825fdc698ea7e834193
TinyKenny <Jonathan@rawet.se>
2018-10-17 17:45:14 +0200

    adding mathhelper

---
+++ project_wednesday/Assets/Scripts/MathHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathHelper {
+
+    public const float FloatEpsilon = 0.001f;
+
+    public static int Sign(float value)
+    {
+        return value > 0.0f ? 1 : Mathf.Abs(value) < FloatEpsilon ? 0 : -1;
+    }
+
+}
